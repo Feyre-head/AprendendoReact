f18973f8578e98b1243e898cca887fd87ae28d93
Inara Oenning <inaraoenning@gmail.com>
2025-07-09 18:03:20 -0300

    Fetch Aprendendo React

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Message from './Message'
+import Header from './header'
+
+
+function App() {
+  return(
+   
+    <div>
+      <Header></Header>
+      <Message></Message>
+    </div>
+  )
+
+}
+
+export default App;
---
+++ src/header.tsx
@@ -0,0 +1,8 @@
+function Header(){
+
+   return (<img src="https://i.pinimg.com/736x/2d/81/53/2d81538bb7c18d5519c1a0b95c5937b0.jpg" alt="" />
+    
+   )
+}
+
+export default Header;
